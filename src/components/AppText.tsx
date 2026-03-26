c7211d1c6ba9f6dc19868dcf3e9ae5cdc18d4594
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-03-26 14:45:01 +0500

    add a reusable component for text

---
+++ src/components/AppText.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, TextProps, TextStyle } from "react-native";
+import React, { FC } from "react";
+import { s } from "react-native-size-matters";
+
+interface AppTextProps extends TextProps {
+  children: React.ReactNode;
+  style?: TextStyle | TextStyle[];
+  variants?: "bold" | "medium";
+}
+
+const AppText: FC<AppTextProps> = ({
+  children,
+  style,
+  variants = "medium",
+  ...rest
+}) => {
+  return (
+    <Text {...rest} style={[styles[variants], style]}>
+      {children}
+    </Text>
+  );
+};
+
+export default AppText;
+
+const styles = StyleSheet.create({
+  bold: {
+    fontSize: s(18),
+    color: "#000",
+  },
+  medium: {
+    fontSize: s(16),
+    color: "#000",
+  },
+});
