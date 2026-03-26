5192417823cc6f1ed07b21b49ad80d1b8ec8e31d
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-03-26 15:20:22 +0500

    addflash message library for showing success and errors

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import AppText from "./src/components/AppText";
 import { AppColors } from "./src/styles/colors";
+import AppSafeView from "./src/views/AppSafeView";
+import FlashMessage, { showMessage } from "react-native-flash-message";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppText variants="medium">Hello </AppText>
-      <AppText variants="bold">Hi there</AppText>
-    </View>
+    <>
+      <FlashMessage position={"top"} />
+      <AppSafeView style={styles.container}>
+        <AppText variants="medium">Hello </AppText>
+        <AppText variants="bold">Hi there</AppText>
+        <Button
+          title="Hello World"
+          onPress={() => {
+            showMessage({
+              message: "Hello there how are you?",
+              color: "blue",
+              type: "success",
+            });
+          }}
+        />
+      </AppSafeView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: AppColors.white,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  container: {},
 });
---
+++ src/views/AppSafeView.tsx
@@ -0,0 +1,36 @@
+import {
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  View,
+  ViewStyle,
+} from "react-native";
+import React, { FC, ReactNode } from "react";
+import { AppColors } from "../styles/colors";
+import { IS_ANDROID } from "../constants/constants";
+
+interface AppSaveViewProps {
+  children: ReactNode;
+  style?: ViewStyle | ViewStyle[];
+}
+
+const AppSafeView: FC<AppSaveViewProps> = ({ children, style }) => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, style]}>{children}</View>
+    </SafeAreaView>
+  );
+};
+
+export default AppSafeView;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: AppColors.white,
+    paddingTop: IS_ANDROID ? StatusBar.currentHeight || 0 : 0,
+  },
+  container: {
+    flex: 1,
+  },
+});
