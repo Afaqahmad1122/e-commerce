6677c72398e93cfd12a25030dcaf76c411dc592d
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-03-26 14:56:20 +0500

    added reusable color coponent for the entire app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import AppText from "./src/components/AppText";
+import { AppColors } from "./src/styles/colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppText variants="medium">Hello </AppText>
+      <AppText variants="bold">Hi there</AppText>
     </View>
   );
 }
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: AppColors.white,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
