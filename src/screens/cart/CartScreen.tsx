6de5d35c6b115b3545a6f20cebafb2c39d2aff95
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-03-27 15:00:34 +0500

    added a reusable component

--- src/screens/cart/CartScreen.tsx
+++ src/screens/cart/CartScreen.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import AppSafeView from "../../components/views/AppSafeView";
+import HomeHeader from "../../components/heeaders/HomeHeader";
 
 const CartScreen = () => {
   return (
-    <View>
+    <AppSafeView>
+      <HomeHeader />
       <Text>CartScreen</Text>
-    </View>
+    </AppSafeView>
   );
 };
 
--- src/screens/home/HomeSceen.tsx
+++ src/screens/home/HomeSceen.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import AppSafeView from "../../components/views/AppSafeView";
+import HomeHeader from "../../components/heeaders/HomeHeader";
 
 const HomeSceen = () => {
   return (
-    <View>
+    <AppSafeView>
+      <HomeHeader />
       <Text>HomeSceen</Text>
-    </View>
+    </AppSafeView>
   );
 };
 
